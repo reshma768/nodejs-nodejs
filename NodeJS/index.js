fa57d12337c9691a4dd565cebd18ccd655be04f4
reshma768 <144494726+reshma768@users.noreply.github.com>
2023-12-05 17:24:48 -0500

    Update index.js

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,52 +1,58 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const mime = require('mime-types');
+const { MongoClient } = require('mongodb');
 
-http.createServer((req, res) => {
-    const url = req.url.toLowerCase();
-
-    if (url === '/') {
-        // Home page
+http.createServer(async (req, res) => {
+    console.log(req.url);
+    if (req.url === '/') {
         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-            if (err) {
-                console.error(err);
-                res.writeHead(500, { 'Content-Type': 'text/html' });
-                res.end('<h1>500 Internal Server Error</h1>');
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(content);
-            }
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
         });
-    } else if (url === '/about') {
-        // About page
+    } else if (req.url === '/about') {
         fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-            if (err) {
-                console.error(err);
-                res.writeHead(500, { 'Content-Type': 'text/html' });
-                res.end('<h1>500 Internal Server Error</h1>');
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(content);
-            }
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
         });
-    } else if (url === '/api') {
-        // API endpoint
-        fs.readFile(path.join(__dirname, 'public', 'db.json'), (err, content) => {
-            if (err) {
-                console.error(err);
-                res.writeHead(500, { 'Content-Type': 'text/html' });
-                res.end('<h1>500 Internal Server Error</h1>');
-            } else {
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(content);
-            }
-        });
-    }  else {
-        // Unknown URL
+    } else if (req.url === '/api') {
+        try {
+            const data = await retrieveDataFromMongoDB();
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(data));
+        } catch (error) {
+            console.error(error);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
+        }
+    } else {
         res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('<h1>404 Not Found</h1>');
+        res.end("<h1> 404 Nothing is here </h1>");
     }
-}).listen(3000, () => console.log('Server is running'));
+}).listen(5959, () => console.log("Server is running on port 5959"));
 
+async function retrieveDataFromMongoDB() {
+    const uri = "mongodb+srv://rshai10:R16661666@cluster0.yxifel9.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
 
+    try {
+        await client.connect();
+        const databaseName = "food";
+        const collectionName = "nutrition";
+        const cursor = client.db(databaseName).collection(collectionName).find({});
+        const results = await cursor.toArray();
+        const formattedResults = [];
+
+        results.forEach(result => {
+            result.Food.forEach(foodItem => {
+                formattedResults.push({ _id: result._id, ...foodItem });
+            });
+        });
+
+        return formattedResults;
+    } finally {
+        await client.close();
+    }
+}
