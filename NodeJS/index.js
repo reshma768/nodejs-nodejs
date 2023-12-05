b3a64bd6dcb6699925520830e13d73ccc3008871
reshma768 <144494726+reshma768@users.noreply.github.com>
2023-12-05 01:00:37 -0500

    Update index.js

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,45 +1,65 @@
-const http = require('http') ; 
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
-http.createServer((req, res) =>{
+
+http.createServer((req, res) => {
     console.log(req.url);
-    if(req.url ==='/'){ // home page
 
-       fs.readFile(path.join(__dirname,'public','index.html'),(err,content)=>{
-
-        if (err) throw err ;
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.end(content)
-
-       })
-
+    if (req.url === '/') {
+        // home page
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        });
+    } else if (req.url === '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        });
+    } else if (req.url === '/api') {
+        fs.readFile(path.join(__dirname, 'public', 'db.json'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(content);
+        });
+    } else if (req.url.startsWith('/public/')) {
+        // Serve static files (images, etc.) from the 'public' directory
+        const filePath = path.join(__dirname, req.url);
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>404 Not Found</h1>');
+            } else {
+                const contentType = getContentType(filePath);
+                res.writeHead(200, { 'Content-Type': contentType });
+                res.end(content);
+            }
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 Not Found</h1>');
     }
-    else if (req.url === '/about'){
-        fs.readFile(path.join(__dirname,'public','about.html'),(err,content)=>{
 
-            if (err) throw err ;
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            res.end(content)
-    
-           })
-    
+}).listen(3000, () => console.log("Server is running"));
+
+function getContentType(filePath) {
+    const extname = path.extname(filePath);
+    switch (extname) {
+        case '.html':
+            return 'text/html';
+        case '.css':
+            return 'text/css';
+        case '.js':
+            return 'text/javascript';
+        case '.json':
+            return 'application/json';
+        case '.png':
+            return 'image/png';
+        case '.jpg':
+            return 'image/jpg';
+        default:
+            return 'application/octet-stream';
     }
-    else if (req.url ==='/api'){
-        fs.readFile(path.join(__dirname,'public','db.json'),(err,content)=>{
-
-            if (err) throw err ;
-            res.writeHead(200, {'Content-Type': 'application/json'})
-            res.end(content)
-    
-           })
-    
-    }
-    else{
-        res.end("<h1> 404 Nothing is here </h1>")
-    }
-
-    
-
-}).listen(3000,()=>console.log("Server is running"));
-
- 
+}
