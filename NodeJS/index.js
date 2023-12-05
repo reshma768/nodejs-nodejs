76c5cee0482bb867348741feb0fff784aeeafdd3
reshma768 <144494726+reshma768@users.noreply.github.com>
2023-12-05 11:14:42 -0500

    Update index.js

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,62 +1,70 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const mime = require('mime-types');
 
 http.createServer((req, res) => {
-    console.log(req.url);
-    if (req.url === '/') { // home page
+    const url = req.url.toLowerCase();
+
+    if (url === '/') {
+        // Home page
         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-            if (err) throw err;
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content);
+            if (err) {
+                console.error(err);
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.end('<h1>500 Internal Server Error</h1>');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
         });
-    } else if (req.url === '/about') {
+    } else if (url === '/about') {
+        // About page
         fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-            if (err) throw err;
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content);
+            if (err) {
+                console.error(err);
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.end('<h1>500 Internal Server Error</h1>');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
         });
-    } else if (req.url === '/api') {
+    } else if (url === '/api') {
+        // API endpoint
         fs.readFile(path.join(__dirname, 'public', 'db.json'), (err, content) => {
-            if (err) throw err;
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(content);
+            if (err) {
+                console.error(err);
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.end('<h1>500 Internal Server Error</h1>');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(content);
+            }
         });
-    } else if (req.url.startsWith('/image/')) {
-        // Serve image files from the 'public/image' directory
-        serveImage(req.url.substring(1), res);
+    } else if (url.startsWith('/image/')) {
+        // Serve image files
+        serveImage(url.substring(1), res);
     } else {
-        res.end("<h1> 404 Nothing is here </h1>");
+        // Unknown URL
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 Not Found</h1>');
     }
-}).listen(3000, () => console.log("Server is running"));
+}).listen(3000, () => console.log('Server is running'));
 
 function serveImage(filename, response) {
     const filePath = path.join(__dirname, 'public', 'image', filename);
     fs.readFile(filePath, (err, content) => {
         if (err) {
+            console.error(err);
             response.writeHead(404, { 'Content-Type': 'text/html' });
             response.end('<h1>404 Not Found</h1>');
         } else {
-            // Determine Content-Type based on the file extension
-            const extname = path.extname(filename).toLowerCase();
-            const contentType = getContentType(extname);
+            // Determine Content-Type based on file content
+            const contentType = mime.lookup(filePath) || 'application/octet-stream';
 
             response.writeHead(200, { 'Content-Type': contentType });
             response.end(content);
         }
     });
-}
-
-function getContentType(extname) {
-    switch (extname) {
-        case '.png':
-            return 'image/png';
-        case '.jpg':
-        case '.jpeg':
-            return 'image/jpeg';
-        case '.gif':
-            return 'image/gif';
-        default:
-            return 'application/octet-stream';
-    }
 }
